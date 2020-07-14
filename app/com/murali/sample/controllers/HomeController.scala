c749e574ad95de565371d9c7c0da60fb1c0e4774
muralishub <muralispost@gmail.com>
2020-07-14 15:24:08 +0100

    Revert "dockerise"
    This reverts commit 28d12373d0ff73079a298b07ab21a972af8d9d92.

--- app/com/murali/sample/controllers/HomeController.scala
+++ app/com/murali/sample/controllers/HomeController.scala
@@ -1,13 +1,35 @@
 package com.murali.sample.controllers
 
-import com.murali.sample.views.html._
+import com.murali.sample.models.{Product, ProductForm}
 import javax.inject._
+import com.murali.sample.views.html._
+import play.api.data.Form
 import play.api.mvc._
+import play.api.data.Forms._
 
 @Singleton
 class HomeController  @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
+
+
+  def postUrl = routes.HomeController.registerUser()
+
+
   def showHome = Action { implicit request =>
+
     Ok(index())
+  }
+
+
+
+  def registerUser = Action { implicit  request =>
+
+   ProductForm.productForm.bindFromRequest.fold(
+      formWithErrors => BadRequest,
+     product =>  {
+        product.name
+        println(product.name)
+        Ok("product OK!")
+      })
   }
 }
 
--- build.sbt
+++ build.sbt
@@ -10,13 +10,3 @@ scalaVersion := "2.12.8"
 libraryDependencies += guice
 libraryDependencies += "org.scalatestplus.play" %% "scalatestplus-play" % "4.0.3" % Test
 libraryDependencies += "org.mockito" % "mockito-core" % "2.23.4" % Test
-//
-//import com.typesafe.sbt.packager.docker.DockerChmodType
-//dockerChmodType := DockerChmodType.UserGroupWriteExecute
-
-javaOptions in Universal ++= Seq(
-  "-Dpidfile.path=/dev/null"
-)
-
-// for docker it uses jre instead of jdk which makes it light weight
-dockerBaseImage       := "openjdk:jre"
